285a79e25bd48938ee89c9ba65821859d0c91027
ofek0803 <85187891+ofek0803@users.noreply.github.com>
2024-12-02 16:30:49 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,27 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
-  }
-}
+package listt;
+import java.util.Random;
+public class shior1 {
+	public static Node<Integer> rand(int n,int x,int y){
+		Random rand=new Random();
+		Node<Integer> a=new Node<Integer>(-1);
+		Node<Integer> first=a;
+		Node<Integer> b;
+		for (int i=0;i<n;i++) {
+			b=new Node<Integer>(rand.nextInt(x-y+1)+y);
+			a.setNext(b);
+			a=a.getNext();
+		}
+		return first.getNext();
+	}
+	
+	public static int Howmany(Node<Integer>a, int n) {
+		int count=0;
+		Node<Integer>first=a;
+		while(a!=null) {
+			if (a.getValue()==n) {
+				count++;
+				a=a.getNext();
+			}
+		}
+		return count;
+	}
